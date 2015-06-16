0338dd773d7bd19fa66df655f30851e080329874
awal2 <awal@berkeley.edu>
2015-06-16 03:07:33 -0700

    Deleted 10 lines of redundant code.

--- Collatz.c
+++ Collatz.c
@@ -3,12 +3,11 @@
 #include <stdlib.h> /* home of strtoull */
 
 /* Sample input:
- *
- * gcc -O3 -o okCollatz okCollatz.c
+ * 
+ * gcc -O3 -o Collatz Collatz.c (highly recommended)
  * Computer$ /Collatz 300000000
  * Longest Collatz sequence for first 300000000 positive integers found at 268549803 with length 964.
  */
-
 
 unsigned long long getnext( unsigned long long x );
 
@@ -23,20 +22,15 @@ int main(int argc, char *argv[])
 {
 	unsigned long long biggestYet;
 	int argind;
+
 	for ( argind = 0; argind < argc; argind++ )
 	{
-		unsigned long long intrange;
-		intrange = strtoull(argv[argind], NULL, 10);
-
-		unsigned long long *lengths= malloc((intrange+1) * sizeof(unsigned long long)); /* ptr points to mem. location of lengths array. */
-		memset(lengths, 0, (intrange+1) * sizeof(unsigned long long));
-
-		unsigned long long longestlen[2];
+		const unsigned long long intrange = 1 + strtoull(argv[argind], NULL, 10);
+		unsigned long long *lengths = malloc( (intrange) * sizeof(unsigned long long)); 
+		memset(lengths, 0, (intrange) * sizeof(unsigned long long));
+		unsigned long long longestlen[] = {0, 0};
 		unsigned long long seqindex;
 		unsigned long long origdex;
-
-		longestlen[0] = 0;
-		longestlen[1] = 0;
 
 		for (origdex = 2; origdex <= intrange; origdex++) 
 		{
@@ -58,11 +52,8 @@ int main(int argc, char *argv[])
 				}
 			}
 		}
-		if(longestlen[0] > 0 ){ printf("Longest Collatz sequence for first %llu positive integers found at %llu with length %llu. \n", intrange, longestlen[0], longestlen[1]); }
-	
+		if(longestlen[0] > 0 ){ printf("Longest Collatz sequence for first %llu positive integers found at %llu with length %llu. \n", intrange-1, longestlen[0], longestlen[1]); }
 	free(lengths);
-
 	}
 	return 0;
-
 }
